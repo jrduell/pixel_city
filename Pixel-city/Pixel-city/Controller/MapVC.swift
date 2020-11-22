a040f8806e07a5198d01d141daab7cf23a1dd983
jrduell <jrduell7@gmail.com>
2020-11-22 15:20:11 -0600

    mapkit and basic UI

--- Pixel-city/Pixel-city/Controller/MapVC.swift
+++ Pixel-city/Pixel-city/Controller/MapVC.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import MapKit
 
 class MapVC: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
+    @IBAction func centerMapButtonWasPressed(_ sender: Any) {
+    }
+    
 
+}
 
+extension MapVC: MKMapViewDelegate {
+    
 }
 
