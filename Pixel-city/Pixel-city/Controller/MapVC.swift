c7f390ed40fcdc6622af693ae50afb693a5b5d7d
jrduell <jrduell7@gmail.com>
2020-11-29 20:57:43 -0600

    request location services and center location with ibaction button.

--- Pixel-city/Pixel-city/Controller/MapVC.swift
+++ Pixel-city/Pixel-city/Controller/MapVC.swift
@@ -13,29 +13,48 @@ class MapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
-    
+    let regionRadius: Double = 1000
+    //let authorizationStatus = CLLocationManager.authorizationStatus()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
+        locationManagerDidChangeAuthorization(locationManager)
     }
     @IBAction func centerMapButtonWasPressed(_ sender: Any) {
+        if (locationManager.authorizationStatus == .authorizedAlways || locationManager.authorizationStatus == .authorizedWhenInUse) {
+            centerMapOnUserLocation()
+        }
     }
     
 
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
 extension MapVC: CLLocationManagerDelegate {
-    func configureLocationServices() {
-        
-    }
-    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        let accuracyAuthorization = manager.accuracyAuthorization
+        if #available(iOS 14.0, *) {
+            switch manager.authorizationStatus {
+            case .authorizedAlways , .authorizedWhenInUse:
+                centerMapOnUserLocation()
+                break
+            case .denied, .restricted:
+                break
+            case .notDetermined:
+                locationManager.requestAlwaysAuthorization()
+                break
+            default:
+                break
+            }
+        }
     }
 }
+
