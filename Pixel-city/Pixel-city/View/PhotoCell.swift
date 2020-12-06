19dbca49697a4b46434a784745473c45fa437f38
jrduell <jrduell7@gmail.com>
2020-12-06 12:26:21 -0600

    remove spinner before adding another, create Ui collection view programmatically

---
+++ Pixel-city/Pixel-city/View/PhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.swift
+//  Pixel-city
+//
+//  Created by Jacob Duell on 12/6/20.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
